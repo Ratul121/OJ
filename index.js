b0b28dee2c563af18be70592d6f634dd81ba0853
Ratul121 <40421171+Ratul121@users.noreply.github.com>
2023-11-12 13:01:09 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,50 @@
+const express = require('express')
+const axios = require('axios');
+const app = express()
+const port = 3000
+
+app.use(express.json());
+
+app.post('/c', (req, res) => {
+    let data = JSON.stringify({
+        "type": "code",
+        "visibility": "public",
+        "properties": {
+          "language": "c",
+          "files": [
+            {
+              "name": "Main.c",
+              "content": req.body.code
+            }
+          ],
+          "stdin": req.body.input
+        }
+      });
+      
+      let config = {
+        method: 'post',
+        maxBodyLength: Infinity,
+        url: 'https://onecompiler.com/api/code/exec',
+        headers: { 
+          'content-type': 'application/json'
+        },
+        data : data
+      };
+      
+      axios.request(config)
+      .then((response) => {
+
+        res.send(response.data)
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+
+      
+})
+
+app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
+
+
+
+
