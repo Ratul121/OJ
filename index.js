37319f9014986df936e014ee21e46740c1e5b4c6
Ratul121 <40421171+Ratul121@users.noreply.github.com>
2023-11-17 11:26:38 +0600

    test

--- index.js
+++ index.js
@@ -2,8 +2,10 @@ const express = require('express')
 const axios = require('axios');
 const app = express()
 const port = 3000
+var cors = require('cors')
 
 app.use(express.json());
+app.use(cors())
 
 app.post('/c', (req, res) => {
     let data = JSON.stringify({
